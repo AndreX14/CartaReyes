469e8f4e181ef8f78e4d009526230b42732048b0
AndreX14 <38482388+AndreX14@users.noreply.github.com>
2021-01-06 08:01:16 -0600

    Add files via upload

---
+++ estrella.js
@@ -0,0 +1,15 @@
+setInterval(function(){ 
+    $("#estrella1").fadeIn(800).fadeOut(800).fadeIn(800);
+}, 800);
+
+setInterval(function(){ 
+    $("#estrella2").fadeIn(800).fadeOut(800).fadeIn(800);
+}, 600);
+
+setInterval(function(){ 
+    $("#estrella3").fadeIn(800).fadeOut(800).fadeIn(800);
+}, 400);
+
+setInterval(function(){ 
+    $("button").fadeIn(800).fadeOut(800).fadeIn(800);
+}, 3000);
